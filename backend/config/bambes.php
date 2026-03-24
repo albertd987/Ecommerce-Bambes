00d35b68a51020e12a444a72c6b5e6d0b326fe09
root <root@vps-8ed9e57e.vps.ovh.net>
2026-03-24 19:09:44 +0000

    feat: add bambes config with available shoe sizes

---
+++ backend/config/bambes.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    /*
+     * Available shoe sizes shown in the backoffice color management page.
+     * Modify this list to add or remove sizes for all products.
+     */
+    'sizes' => ['36', '37', '38', '39', '40', '41', '42', '43', '44', '45', '46', '47'],
+];
